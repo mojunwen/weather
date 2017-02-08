a1740420f08f51f655674fe4b14e64578ce6ea63
xcas121 <1628242411@qq.com>
2017-02-08 18:24:08 +0800

    修改模块二

--- src/component/two.vue
+++ src/component/two.vue
@@ -1,8 +1,15 @@
 <template>
 	<div id="two">
 		<h2>第二个模块</h2>
-		<div>
-			<h3>和风天气</h3>
+		<h3>和风天气</h3>
+		<div class="contentBox">
+			<nav class="clearfix">
+				<a class="menu">三</a>
+				<div class="nav_box_right">
+					<span>6°</span>
+					<i>☀</i>
+				</div>
+			</nav>
 			
 		</div>
 	</div>
@@ -38,5 +45,28 @@
 </script>
 
 <style>
-	
+	#two * {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+	#two .contentBox {
+		display: block;
+		border: 1px solid #ccc;
+	    width: 400px;
+	    margin: 0 auto;
+	    box-shadow: 10px 10px 10px;
+	    padding: 4px 4px;
+	}
+	#two nav .menu {
+		width: 30px;
+		float: left;
+		cursor: pointer;
+	}
+	#two nav .nav_box_right {
+		width: 60px;
+		float: right;
+	}
+	.clearfix:after{content:".";display:block;height:0;clear:both;visibility:hidden}
+	.clearfix{*+height:1%;}
 </style>
