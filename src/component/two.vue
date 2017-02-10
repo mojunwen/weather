c4f1e8a9a182a4b028f3324568c31933de9ad3c1
unknown <>
2017-02-10 22:22:23 +0800

    添加选择城市功能

--- src/component/two.vue
+++ src/component/two.vue
@@ -2,15 +2,44 @@
 	<div id="two">
 		<h2>第二个模块</h2>
 		<h3>和风天气</h3>
+		<div>
+			<label for="cityBox">输入城市</label>
+			<input id="cityBox" type="text" name="" v-bind:value="city" @change="loopa">
+		</div>
+		<div>
+			<label for="selectBox">选择方法</label>
+			<select @change="changeRouter">
+				<option value="now">现在</option>
+				<option value="forecast">7--10天</option>
+			</select>
+		</div>
 		<div class="contentBox">
 			<nav class="clearfix">
 				<a class="menu">三</a>
 				<div class="nav_box_right">
-					<span>6°</span>
-					<i>☀</i>
+					<span> {{ now.tmp + '°' }} </span>
+					<i><img v-bind:src="rightIcon"></i>
 				</div>
 			</nav>
-			
+			<section>
+				<h2> {{ basic.city }} </h2>
+				<p>
+					<img v-bind:src="rightIcon">
+					<span> {{ now.tmp + '°' || '温度'}} </span>
+				</p>
+				<p>
+					<ul>
+						<li>
+							<label>风力</label>
+							<span>{{now.wind.sc + '级' || '风力'}}</span>
+						</li>
+						<li>
+							<label>风向</label>
+							<span>{{now.wind.dir || '风向'}}</span>
+						</li>
+					</ul>
+				</p>
+			</section>
 		</div>
 	</div>
 
@@ -20,26 +49,47 @@
 	export default {
 		data () {
 			return {
-				items: null,
-				dataUrl: 'https://free-api.heweather.com/v5/hourly'
+				basic: {city: '城市'},
+				now: {tmp: '温度',wind: {sc: '风力', dir: '风向'}},
+				status: null,
+				city: '包头',
+				router: 'now',
+				dataUrl: 'https://free-api.heweather.com/v5/',
+				rightIcon: 'http://files.heweather.com/cond_icon/100.png'
 			}
 		},
 		methods: {
 			getJsonP: function(_url){
-				this.$http.get(_url,{
+				var that = this;
+				this.$http.get(that.dataUrl + _url,{
 					params: {
-						city: '包头',
+						city: that.city,
 						key: '146ba7b820344558a47496c09c16ff6d'
 					}
 				}).then(function(response){
-					this.items = response.body.HeWeather5
+					console.log();
+					if(response.body && response.body.HeWeather5[0].status == 'ok'){
+						this.basic = response.body.HeWeather5[0].basic;
+						this.now = response.body.HeWeather5[0].now;
+						this.status = response.body.HeWeather5[0].status;
+						this.rightIcon = 'http://files.heweather.com/cond_icon/'+this.now.cond.code+'.png'
+					}
+					
 				},function(response){
 					console.log('请求失败');
 				});
+			},
+			loopa : function(e){
+				this.city = e.target.value;
+				this.getJsonP(this.router);
+			},
+			changeRouter : function(e){
+				this.router = e.target.options[e.target.selectedIndex].value;
+				this.getJsonP(this.router);
 			}
 		},
-		mounted: function(){
-			this.getJsonP(this.dataUrl);
+		created: function(){
+			this.getJsonP(this.router);
 		}
 	}
 </script>
@@ -66,6 +116,9 @@
 	#two nav .nav_box_right {
 		width: 60px;
 		float: right;
+	}
+	#two nav .nav_box_right i img {
+		width: 14px;
 	}
 	.clearfix:after{content:".";display:block;height:0;clear:both;visibility:hidden}
 	.clearfix{*+height:1%;}
